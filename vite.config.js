4c1e4665834c65aa516eb7726aaca84c6e8a082d
Blast101 <119773274+Blast101@users.noreply.github.com>
2023-12-21 15:19:30 +0530

    First commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Tic-Tac-Toe-Game/",
+  base:"/Tic-Tac-Toe-Game2/",
   plugins: [react()],
 })
